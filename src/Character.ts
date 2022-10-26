73cc036ef292496ebe5ac929cfc7b9490289651c
Matheus <stvgamer74@gmail.com>
2022-10-25 22:04:10 -0300

    fix: :bug: character class accept in method attack simplefighter

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -44,7 +44,7 @@ export default class Character implements Fighter {
     return this.lifePoints;
   }
 
-  attack(enemy: Fighter) {
+  attack(enemy: SimpleFighter) {
     enemy.receiveDamage(this.strength);
   }
 
